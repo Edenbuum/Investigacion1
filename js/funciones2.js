95b4e8f8f0bd2fadaf50d82aef173ee36e0c4358
Cat <91883499+Edenbuum@users.noreply.github.com>
2022-02-07 09:23:45 +0100

    Add files via upload

---
+++ js/funciones2.js
@@ -0,0 +1,4 @@
+function cargarPagina()
+{
+	document.getElementsByTagName("body")[0].innerHTML="<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione asperiores sunt, aspernatur dicta reprehenderit a corporis, molestiae ullam neque quod molestias. Sit non libero eaque assumenda quos soluta accusamus pariatur? </p>";
+}
